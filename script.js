518080bd5726a27851942b5c1c0df35a786d83c7
Vitor Hugo Rodrigues dos Santos <vitorhrds@MacBook-Air-de-Vitor-2.local>
2024-11-23 17:45:38 -0300

    fix: added modal de pedido especifico

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function itemAdicionado() {
     alert("Adicionado com sucesso!");
     window.location.href = "index.html";
 }
+
+function adicionarItem() {
+    const page = document.getElementById("page");
+    page.style.display = "block";
+}
