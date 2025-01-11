64df71c7fcfd834a1656074be098d6c2e8e33a02
Dmizuta <dani.mizuta@gmail.com>
2025-01-11 14:42:17 -0300

    feat:

--- script.js
+++ script.js
@@ -372,7 +372,7 @@ const productData = {
           modal.style.display = "none";
           document.getElementById("quantity").value = ""; // Clear quantity field
         } else {
-          console.error("Failed to add product to order:", addResult.error);
+          console.error("FALHA AO ADICIONAR O PRODUTO:", addResult.error);
           alert(addResult.error || "FALHA AO ADICIONAR O PRODUTO.");
         }
       } else {
