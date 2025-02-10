dc1f69ad35bd69b4f8220727710dad6135b74bef
Dani Mizuta <dani.mizuta@gmail.com>
2025-02-09 21:33:51 -0300

    feat: product filter

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ async function fetchProductData(epoca) {
     }
 }
 
+// Chama a função para buscar produtos da estação padrão ao carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+  fetchProductData('carnaval'); // Defina a estação padrão aqui
+});
+
+
 // Adiciona eventos de clique aos ícones
 document.getElementById("carnaval").addEventListener("click", () => {
     fetchProductData('carnaval'); // Chama a função para buscar produtos de Carnaval
@@ -36,10 +42,6 @@ document.getElementById("hlwn").addEventListener("click", () => {
     fetchProductData('halloween'); // Chama a função para buscar produtos de Halloween
 });
 
-// Chama a função para buscar produtos da estação padrão ao carregar a página
-document.addEventListener("DOMContentLoaded", () => {
-    fetchProductData('carnaval'); // Defina a estação padrão aqui
-});
 
 
 
