88776b8508ba4d58a029270b8f9cd1e668b1644c
Dani Mizuta <dani.mizuta@gmail.com>
2025-02-24 22:13:00 -0300

    feat:refactoring tests

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ document.getElementById("hlwn").addEventListener("click", function() {
 window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
       document.getElementById("carnaval").click();
-  }, 150); // Delay in milliseconds (500ms = 0.5s)
+  }, 180); // Delay in milliseconds (500ms = 0.5s)
 });
 
 
